20347907f9135b7dbf80f4a56c87971e615214a9
IsmaLB <61549609+Isma-LB@users.noreply.github.com>
2023-02-11 10:34:55 -0500

    Player prefab and movement

---
+++ Memories/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField, Range(0f,10f) ] float moveSpeed = 5f; 
+    Rigidbody2D rb;
+    Vector2 input;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+        input.Normalize();
+        Debug.Log(input);
+    }
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + input * moveSpeed * Time.fixedDeltaTime);
+    }
+}
