bda7266a53236de14425f718d1b6c96d0c14f673
pattodiablo <pattodiablo@gmail.com>
2023-02-11 15:24:16 -0500

    initial camera and win camera

--- Memories/Assets/S_CameraController.cs
+++ Memories/Assets/S_CameraController.cs
@@ -1,15 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class S_CameraController : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    [SerializeField] private CinemachineVirtualCamera TopCamera;
+    [SerializeField] private CinemachineVirtualCamera OnEnterCamera;
+    [SerializeField] private CinemachineVirtualCamera WinCamera;
+
+  
     void Start()
     {
-        
+        TopCamera.gameObject.SetActive(true);
+        OnEnterCamera.gameObject.SetActive(false);
+        WinCamera.gameObject.SetActive(false);
     }
 
+
+    public void winCamera()
+    {
+        TopCamera.gameObject.SetActive(false);
+        OnEnterCamera.gameObject.SetActive(false);
+        WinCamera.gameObject.SetActive(true);
+    }
     // Update is called once per frame
     void Update()
     {
